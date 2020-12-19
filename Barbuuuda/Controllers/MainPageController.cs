03b9efe6e50b039d5d9eb3e5bab9a56e6829f779
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-12-19 14:45:26 +0300

    Добавьте файлы проекта.

---
+++ Barbuuuda/Controllers/MainPageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barbuuuda.Controllers {
+    [ApiController, Route("api/main")]
+    public class MainPageController : ControllerBase {
+
+    }
+}
