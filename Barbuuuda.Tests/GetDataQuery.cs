04f0d800c4b6f6e1b80016208fd4b741dfe2b5c3
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2021-01-16 17:39:29 +0300

    Was added unit tests.

---
+++ Barbuuuda.Tests/GetDataQuery.cs
@@ -0,0 +1,30 @@
+﻿using Barbuuuda.Core.Data;
+using Barbuuuda.Models.Task;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Barbuuuda.Tests {
+    /// <summary>
+    /// Класс получения тестовых данных.
+    /// </summary>
+    public class GetDataQuery {
+        ApplicationDbContext _db;
+        PostgreDbContext _postgre;
+
+        public GetDataQuery(ApplicationDbContext db, PostgreDbContext postgre) {
+            _db = db;
+            _postgre = postgre;
+        }
+
+        /// <summary>
+        /// Метод получает список тестовых заданий.
+        /// </summary>
+        /// <returns></returns>
+        public IList GetTasksList() {
+            return _postgre.Tasks.ToList();
+        }
+    }
+}
---
+++ Barbuuuda.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Barbuuuda.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
