7b4737e426a7818f8dbf1f838cee0089fa19bba7
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2021-03-03 00:03:37 +0300

    Was added unit-test to adding executor questions.

--- Barbuuuda.Core/Data/AuthOptions.cs
+++ Barbuuuda.Core/Data/AuthOptions.cs
@@ -9,7 +9,7 @@ namespace Barbuuuda.Core.Data {
         public const string ISSUER = "MyAuthServer"; // Издатель токена.
         public const string AUDIENCE = "MyAuthClient"; // Потребитель токена.
         const string KEY = "mysupersecret_secretkey!123";   // Ключ для шифрования.
-        public const int LIFETIME = 10; // Время жизни токена.
+        public const int LIFETIME = 15; // Время жизни токена.
 
         public static SymmetricSecurityKey GetSymmetricSecurityKey() {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
