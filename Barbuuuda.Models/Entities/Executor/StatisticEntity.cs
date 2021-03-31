2ee40e9af7591b391b538168608fdbef88ec4d4a
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2021-03-31 21:38:56 +0300

    [bar-g93h37] Was created table executor statistic.

---
+++ Barbuuuda.Models/Entities/Executor/StatisticEntity.cs
@@ -0,0 +1,66 @@
+﻿using Barbuuuda.Models.User;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Barbuuuda.Models.Entities.Executor
+{
+    /// <summary>
+    /// Класс сопоставляется с таблицей статистики исполнителей.
+    /// </summary>
+    [Table("ExecutorStatistic")]
+    public sealed class StatisticEntity
+    {
+        [Key]
+        public int StatId { get; set; }
+
+        /// <summary>
+        /// Id пользователя.
+        /// </summary>
+        public string Id { get; set; }
+
+        [ForeignKey("Id")]
+        public UserEntity UserId { get; set; }
+
+        /// <summary>
+        /// Общее кол-во выполненных заданий.
+        /// </summary>
+        [Column("CountTotalCompletedTask", TypeName = "bigint")]
+        public long? CountTotalCompletedTask { get; set; }
+
+        /// <summary>
+        /// Кол-во положительных отзывов.
+        /// </summary>
+        [Column("CountPositive", TypeName = "bigint")]
+        public long? CountPositive { get; set; }
+
+        /// <summary>
+        /// Кол-во отрицательных отзывов.
+        /// </summary>
+        [Column("CountNegative", TypeName = "bigint")]
+        public long? CountNegative { get; set; }
+
+        /// <summary>
+        /// Рейтинг.
+        /// </summary>
+        [Column("Rating", TypeName = "numeric(12,2)")]
+        public decimal Rating { get; set; }
+
+        /// <summary>
+        /// Счет.
+        /// </summary>
+        [Column("Score", TypeName = "numeric(12,2)")]
+        public decimal Score { get; set; }
+
+        /// <summary>
+        /// Код категории.
+        /// </summary>
+        [Column("CategoryCode", TypeName = "varchar(100)")]
+        public string CategoryCode { get; set; }
+
+        /// <summary>
+        /// Кол-во выполненных заданий категории.
+        /// </summary>
+        [Column("CountTaskCategory", TypeName = "bigint")]
+        public long? CountTaskCategory { get; set; }
+    }
+}
---
+++ Barbuuuda.Models/Entities/User/StateEntity.cs
@@ -0,0 +1,30 @@
+﻿using Barbuuuda.Models.User;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Barbuuuda.Models.Entities.User
+{
+    /// <summary>
+    /// Класс сопоставляется с таблицей статистики пользователей.
+    /// </summary>
+    [Table("State")]
+    public sealed class StateEntity
+    {
+        [Key]
+        public int StateId { get; set; }
+
+        /// <summary>
+        /// Id пользователя.
+        /// </summary>
+        public string Id { get; set; }
+
+        [ForeignKey("Id")]
+        public UserEntity UserId { get; set; }
+
+        /// <summary>
+        /// Флаг нахождения на сервисе в данный момент.
+        /// </summary>
+        [Column("IsOnline", TypeName = "bool")]
+        public bool IsOnline { get; set; }
+    }
+}
